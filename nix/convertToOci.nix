80cc063688042ec455494b96a1c1441a7aa1a45c
Alex Suraci <suraci.alex@gmail.com>
2022-08-01 12:00:19 -0400

    wip: pivot around nixery

--- nix/convertToOci.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { }
-, image
-}:
-pkgs.runCommand "convert-to-oci"
-{
-  nativeBuildInputs = [ pkgs.skopeo ];
-} ''
-  ${image} | gzip --fast | skopeo --tmpdir $TMPDIR --insecure-policy copy --quiet docker-archive:/dev/stdin oci-archive:$out:latest
-''
