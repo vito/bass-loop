2af5de2868a256fa681c5e796078e32c973a9bdf
Alex Suraci <suraci.alex@gmail.com>
2022-08-07 22:58:37 -0400

    restore nix dev shell

--- flake.nix
+++ flake.nix
@@ -3,8 +3,38 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    { };
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        packages = {
+          default = pkgs.callPackage ./default.nix { };
+        };
+
+        defaultApp = {
+          type = "app";
+          program = "${packages.bass-loop}/bin/bass-loop";
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = pkgs.callPackage ./nix/deps.nix { } ++ (with pkgs; [
+              gopls
+            ]);
+          };
+        };
+      });
 }
